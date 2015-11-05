6f256d738e3cf0242c3f68e8c38de00b0c22aff1
drowsy810301 <lbj39919538@gmail.com>
2015-11-05 16:13:51 +0800

    add tag class

--- photos/admin.py
+++ photos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from photos.models import Photo
+from photos.models import Photo, Tag
 # Register your models here.
 admin.site.register(Photo)
-
+admin.site.register(Tag)
--- photos/models.py
+++ photos/models.py
@@ -4,13 +4,19 @@ from django.conf import settings
 from users.models import Account
 
 # Create your models here.
+class Tag(models.Model):
+
+    tag_name = models.CharField(max_length=20, default='')
+    def __unicode__(self):
+        return self.tag_name
 
 class Photo(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField(default=None)
     #related_name can reverse foreign krey to one-to-many
     owner = models.ForeignKey(Account, related_name='photos')
-    #upload_time = models.DateTimeField(default=datetime.now)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+
     Flickr_vote = models.IntegerField()
     FB_vote = models.IntegerField()
     image = models.FileField(upload_to='uploads/images')
@@ -18,3 +24,5 @@ class Photo(models.Model):
 
     def __unicode__(self):
         return str(self.title)
+
+
