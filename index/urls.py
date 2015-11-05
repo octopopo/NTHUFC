8502a3bc47b50ecb31854329dae86b09019e7912
drowsy810301 <lbj39919538@gmail.com>
2015-11-05 22:16:55 +0800

    add login modal

--- index/urls.py
+++ index/urls.py
@@ -3,5 +3,5 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^sign_up/$', views.sign_up, name='sign_up'),
+    url(r'^participate/$', views.participate, name='participate'),
 )
