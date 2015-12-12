af222a7dfdc3af3962fa344c3fbad336ef444531
drowsy810301 <lbj39919538@gmail.com>
2015-12-12 15:24:46 +0800

    modify model

--- photos/models.py
+++ photos/models.py
@@ -7,8 +7,17 @@ from users.models import Account
 class Tag(models.Model):
 
     tag_name = models.CharField(max_length=20, default='')
+    tag_count = models.IntegerField(default=0)
     def __unicode__(self):
         return self.tag_name
+
+class LocationMarker(models.Model):
+    location_text = models.CharField(max_length=10)
+    latitude = models.DecimalField(max_digits=18, decimal_places=15);
+    longitude = models.DecimalField(max_digits=18, decimal_places=15);
+
+    def __unicode__(self):
+        return self.location_text;
 
 class Photo(models.Model):
     title = models.CharField(max_length=30)
@@ -17,11 +26,11 @@ class Photo(models.Model):
     owner = models.ForeignKey(Account, related_name='photos')
     tags = models.ManyToManyField(Tag, blank=True, null=True)
 
-    Flickr_vote = models.IntegerField(default=0)
-    FB_vote = models.IntegerField(default=0)
+    location_marker = models.ForeignKey(LocationMarker)
+    flickr_photo_id = models.CharField(max_length=50)
+    facebook_post_id = models.CharField(max_length=50)
     upload_time = models.DateTimeField(default=datetime.now, blank=False, null=False)
     image = models.ImageField(upload_to='uploads/images')
-
 
     def __unicode__(self):
         return self.title
